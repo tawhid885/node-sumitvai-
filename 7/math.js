d0bccdfc430273a9c8f933a15ca2068283ae4259
unknown <hmdshakil885@gmail.com>
2022-03-14 00:52:50 +0600

    comment done

--- 7/math.js
+++ 7/math.js
@@ -1,9 +1,17 @@
+// Author : Md. Shakil Hossen 
+// Title : Node Simple Practise 
+// Data : 3/14/2022 
+
+
+// scaffolder 
 const math ={};
 
+// random value generate 
 math.randomNumber = function randomNumber(max){
     const maximum = max;
     const result = Math.floor(Math.random()*max)
     return result;
 }
 
+// export math 
 module.exports = math;
--- 7/quote.js
+++ 7/quote.js
@@ -1,11 +1,23 @@
+// Author : Md. Shakil Hossen 
+// Title : Node Simple Practise 
+// Data : 3/14/2022 
+
+
+// dependency 
 const fs = require("fs");
 
+// quote.txt file read 
 const myFile = fs.readFileSync("quote.txt", "utf-8");
 
+// scaffolder 
 const quote={};
+
+// quote file read and make it array 
 quote.quoteFile = function quoteFile(){
     return myFile.split(/\r?\n/);
 };
 
+
+// export quote 
 module.exports = quote;
 
